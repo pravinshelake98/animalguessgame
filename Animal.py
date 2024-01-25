314e86419992239471a9ac0100bd678208b319b2
pravin shelake <pravinshelake1997@gmail.com>
2024-01-25 21:05:51 +0530

    just some cahnges

--- Animal.py
+++ Animal.py
@@ -67,16 +67,5 @@ def animal_says(animal):
         return "Goat says: Beeee"
     else:
         return "Unknown animal"
-    
-user_input = input("Choose a set of animals (a, b, c): ")
-
-animals = []
-for i in range(3):
-    random_animal = random.choice([Cow(), Monkey(), Dog(),Lion(),Goat(),Tiger(),Bull(),Elephant()])
-    animals.append(random_animal)
-
-for animal in animals:
-    print(animal_says(animal))
-
 
 
---
+++ main.py
@@ -0,0 +1,9 @@
+user_input = input("Choose a set of animals (a, b, c): ")
+
+animals = []
+for i in range(3):
+    random_animal = random.choice([Cow(), Monkey(), Dog(),Lion(),Goat(),Tiger(),Bull(),Elephant()])
+    animals.append(random_animal)
+
+for animal in animals:
+    print(animal_says(animal))
